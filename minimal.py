2166e39afc6db941717f94bf17386168999dc5b8
rachelndlc <58004269+rachelndlc@users.noreply.github.com>
2019-12-20 08:22:55 -0500

    example grid code

--- minimal.py
+++ minimal.py
@@ -7,6 +7,49 @@ pygame.init()
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 300) )
 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 20
+HEIGHT = 20
+ 
+# This sets the margin between each cell
+MARGIN = 5
+ 
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+grid = []
+for row in range(10):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell
+ 
+# Set row 1, cell 5 to one. (Remember rows and
+# column numbers start at zero.)
+grid[1][5] = 1
+ 
+# Initialize pygame
+pygame.init()
+ 
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [255, 255]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+ 
+# Set title of screen
+pygame.display.set_caption("Array Backed Grid")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
 # Main loop. Your game would go inside this loop
 while True:
     # do something for each event in the event queue (list of things that happen)
--- text.py
+++ text.py
@@ -4,6 +4,10 @@ import pygame
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
+red = pygame.Color(255,0)
+font = pygame.font.SYsFont("Garmond", 100)
+Text = font.render("Rachel", True, red)
+
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 300) )
 
@@ -11,9 +15,12 @@ screen = pygame.display.set_mode( (400, 300) )
 while True:
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
+        print(event)
         # Check to see if the current event is a QUIT event
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
-screen.fill(red)
+
+#fill the screen with color
+screen.blit(text, pygame.mouse
 pygame.display.flip()
